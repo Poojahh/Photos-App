d2d581ed0e832ba86d65520e267a48b678376668
Poojahh <poojakrishnan1104@gmail.com>
2024-07-24 20:12:36 +0530

    Photos App

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const View = () => {
  const [data,changeData]=useState(
@@ -35017,6 +35018,7 @@ const View = () => {
  )   
     return (
         <div>
+            <Navbar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
